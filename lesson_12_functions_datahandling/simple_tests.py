4416aaaaf5a2430925ea00e7636119c9a4ac78ac
Kostiantyn Lemeshok <klemeshok@gmail.com>
2020-11-30 21:13:16 +0200

    hometask to lesson_13 uploaded

--- lesson_12_functions_datahandling/simple_tests.py
+++ lesson_12_functions_datahandling/simple_tests.py
@@ -1,10 +1,26 @@
 # coding: utf-8
 
+from urllib import parse
+
 
 def word_count(phrase):
-	return len(phrase.split(' '))
+    if phrase:
+        return len(phrase.split(' '))
+    else:
+        return 0
+word_count('')
+
+# если это файлик для автотестирования, прежде чем тестировать функцию, ее надо импортировать из файла, где она лежит
+# например, она лежит в файлике useful_functions.py (при импорте расширение не указываем)
+
+from useful_functions import word_count
 
 
 def test_word_count():
 	assert word_count('мрт менделеевская') == 2
 	assert word_count('') == 0
+
+def campaign_name(url):
+
+
+
